30a3b27064a730627d4d1674be59595749bea521
203461 <78136637+203461@users.noreply.github.com>
2022-03-14 20:54:04 -0600

    old files deleted

--- Primer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from django.conf.urls import include
-
-
-from Primer.views import PrimerTablaList
-
-urlpatterns = [
-    re_path(r'^lista/$', PrimerTablaList.as_view()),    
-]
