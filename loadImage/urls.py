108694cbb1e9f34f815d1d2b8a19f532742cd755
203461 <78136637+203461@users.noreply.github.com>
2022-03-14 20:53:51 -0600

    old files deleted

--- loadImage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from django.conf.urls import include
-from loadImage.views import imageView
-from loadImage.views import imagenViewDetail
-
-urlpatterns = [
-    re_path(r'^archivoImagen/$', imageView.as_view()),
-    re_path(r'^archivoImagen/(?P<pk>\d+)$', imagenViewDetail.as_view()),
-]
